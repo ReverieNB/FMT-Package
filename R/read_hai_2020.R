8f7548be59b59bd9e95522df3143879808bdd048
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2021-11-02 11:38:16 -0500

    Small edits
    Plus roboto function

--- R/read_hai_2020.R
+++ R/read_hai_2020.R
@@ -10,24 +10,28 @@
 
 read_hai_2020 <- function(folder, quarter, year=2020, joinMOU=FALSE) {
   hai_measures <- c("CAUTI", "CDIFF", "CLABSI", "MRSABLD", "SSICOLO", "SSIHYST")
-  hai_data <- tibble::tibble()
+  hai_data <- data.frame()
 
   for (m in hai_measures){
-    file_data <- readr::read_csv(paste0("//files.umn.edu/sph/HPM/Projects/RHRC/RHRC_Flex/Data/MBQIP Data/", year, "/", folder, "CAH_HAI_", m, "_", year, "Q", quarter,".csv")) %>%
-      janitor::clean_names() %>%
-      dplyr::select(provider_id, hsp_state, subm_qtr, sir_num, sir_den, sir,
-             days_surg= ifelse(stringr::str_detect(m, "SSI")==TRUE, tidyselect::contains("surg"), tidyselect::contains("days")), hsp_name, hsp_city, hsp_state) %>%
-      dplyr::rename(num= sir_num,
+    file_data <- read_csv(paste0(fmt_folder, "Data/MBQIP Data/", year, "/", folder, "HRSA_CAH_HAI_", m, "_", year, "Q", quarter,".csv")) %>%
+      clean_names() %>%
+      select(provider_id, hsp_state, subm_qtr, sir_num, sir_den, sir,
+             days_surg= ifelse(str_detect(m, "SSI")==TRUE, contains("surg"), contains("days")), hsp_name, hsp_city, hsp_state) %>%
+      rename(num= sir_num,
              den= sir_den) %>%
-      dplyr::mutate(measure= m,
+      mutate(measure= m,
              provider_id= as.numeric(provider_id),
-             dplyr::across(num:den, ~ifelse(is.na(.x), 0, .x)))
-    #need to make these 0 instead of NA as long as CAH is included in data.. see more in OPIP report notes
+             across(num:den, ~ifelse(is.na(.x), 0, .x))) %>%
+      #need to make these 0 instead of NA as long as CAH is included in data.. see more in OPIP report notes
+      {if (quarter==1) filter(.data= ., provider_id %ni% nomou_p1) else .} %>%
+      {if (quarter==2) filter(.data= ., provider_id %ni% nomou_p2) else .} %>%
+      {if (quarter==3) filter(.data= ., provider_id %ni% nomou_p3) else .} %>%
+      {if (quarter==4) filter(.data= ., provider_id %ni% nomou_p4) else .}
 
     if (joinMOU==TRUE){
       file_data <- moulist %>%
         left_join(file_data, by= "provider_id") %>%
-        dplyr::mutate(measure= ifelse(is.na(measure)==TRUE, m, measure))
+        mutate(measure= ifelse(is.na(measure)==TRUE, m, measure))
     }
 
     hai_data <- rbind(hai_data, file_data)
