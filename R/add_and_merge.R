3df1206f24de8e912862c86b6f6a84a4fb6bf7bf
ReverieNB <62551793+ReverieNB@users.noreply.github.com>
2022-03-23 09:28:48 -0500

    rewrote add_and_merge

--- R/add_and_merge.R
+++ R/add_and_merge.R
@@ -1,17 +1,27 @@
 #' Add content to spreadsheets
 #'
-#' Similar to add_stuff but will also merge columns
+#' Similar to add_stuff, but can add values to merged rows and columns
 #' @param value the value to add
 #' @param sheet sheet name
-#' @param row_num cell row number
-#' @param colIndex minimum column
-#' @param max_col maximum column
+#' @param row_num minimum row number
+#' @param col_num minimum column
+#' @param height number of merged rows
+#' @param width number of merged columns
 #' @param style formatting style
 #' @export
 
-add_and_merge <- function(value, sheet, row_num, colIndex, max_col, style= column_title){
-  sheet_value <- xlsx::createCell(rows[row_num], colIndex = colIndex)
+add_and_merge <- function(value, sheet, row_num, col_num, height=0, width=0, style= column_title){
+  sheet_value <- xlsx::createCell(rows[row_num], colIndex = col_num)
   xlsx::setCellValue(sheet_value[[1,1]], value)
   xlsx::setCellStyle(sheet_value[[1,1]], style)
-  xlsx::addMergedRegion(sheet, row_num, row_num, colIndex, max_col) #start and end row must always be the same
+  xlsx::addMergedRegion(sheet, row_num, ifelse(height==0, row_num, row_num+height-1), col_num, ifelse(width==0, col_num, col_num+width-1))
 }
+
+#added height/width 3/23/22.. old function, which doesnt have vertical merging and uses max_col instead of width. also renamed colIndex
+
+#add_and_merge <- function(value, sheet, row_num, colIndex, max_col, style= column_title, height=0){
+#  sheet_value <- xlsx::createCell(rows[row_num], colIndex = colIndex)
+#  xlsx::setCellValue(sheet_value[[1,1]], value)
+#  xlsx::setCellStyle(sheet_value[[1,1]], style)
+#  xlsx::addMergedRegion(sheet, row_num, ifelse(height==0, row_num, row_num+height-1), colIndex, max_col) #start and end row must always be the same
+#}
